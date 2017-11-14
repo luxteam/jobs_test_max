12d331b2552fc78d58d2ed948b4d9392adf5c64d
Andrey Alekseevich <magister.demens@gmail.com>
2017-11-14 20:23:27 +0300

    simplify refactor

--- jobs/Scripts/simpleRender.py
+++ jobs/Scripts/simpleRender.py
@@ -1,63 +1,10 @@
 import argparse
 import sys
 import os
-
-# sys.path.append(r"..\..\..\CommonScripts\ImageComparator\\")
-import time
 import subprocess
-import multiprocessing
 import psutil
-import ctypes
-from datetime import datetime
 import json
-# import CompareMetrics
-import getpass
-from email.mime.multipart import MIMEMultipart as MM
-from email.mime.text import MIMEText as MT
-import smtplib
-import email
-import email.mime.application
-
-
-def get_windows_titles():
-    EnumWindows = ctypes.windll.user32.EnumWindows
-    EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
-    GetWindowText = ctypes.windll.user32.GetWindowTextW
-    GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
-    IsWindowVisible = ctypes.windll.user32.IsWindowVisible
-
-    titles = []
-
-    def foreach_window(hwnd, lParam):
-        if IsWindowVisible(hwnd):
-            length = GetWindowTextLength(hwnd)
-            buff = ctypes.create_unicode_buffer(length + 1)
-            GetWindowText(hwnd, buff, length + 1)
-            titles.append(buff.value)
-        return True
-
-    EnumWindows(EnumWindowsProc(foreach_window), 0)
-
-    return titles
-
-
-def _late_kill(pid, delay, real_rc=None):
-    proc = psutil.Process(pid)
-    titles = []
-    while True:
-        try:
-            proc.wait(delay)
-        except psutil.TimeoutExpired as e:
-            titles = get_windows_titles()
-            if "Radeon ProRender" in titles:
-                if real_rc:
-                    real_rc.value = -1024
-                for child in reversed(proc.children(recursive=True)):
-                    child.terminate()
-                proc.terminate()
-                return
-        else:
-            return
+# TODO: create
 
 
 def main():
@@ -124,49 +71,26 @@ def main():
     maxScriptPath = os.path.join(work_dir, 'script.ms')
     with open(maxScriptPath, 'w') as f:
         f.write(maxScript)
-        f.closed
 
     cmdRun = '"{tool}" -U MAXScript "{job_script}" -silent' \
         .format(tool=tool, job_script=maxScriptPath)
 
-    cmdRun_ForBat = '"{tool}" -U MAXScript "{job_script}" -silent' \
-        .format(tool=tool, job_script='script.ms')
-
     cmdScriptPath = os.path.join(work_dir, 'script.bat')
     with open(cmdScriptPath, 'w') as f:
-        f.write(cmdRun_ForBat)
-        f.closed
+        f.write(cmdRun)
 
-    print("start cmdRun time: ", time.time())
-    #child = subprocess.Popen(cmdRun, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    os.chdir(work_dir)
 
-    p = psutil.Popen(cmdRun, stdout=subprocess.PIPE)
-    rc = p.wait(timeout=2000)
-
-    #real_rc = multiprocessing.Value('i', 0)
-    #monitor = multiprocessing.Process(target=_late_kill, args=(child.pid, 10, real_rc))
-    #print("start monitor time: ", time.time())
-    #monitor.start()
+    p = psutil.Popen(os.path.join(args.output, 'script.bat'), stdout=subprocess.PIPE)
+    try:
+        rc = p.wait(timeout=2000)
+    except psutil.TimeoutExpired as err:
+        sub_skip = -1
+        for child in reversed(p.children(recursive=True)):
+            child.terminate()
+        p.terminate()
 
     sub_skip = 0
-    #proc = psutil.Process(child.pid)
-    #rc= proc.wait()
-#    while True:
-#        output = child.stdout.readline()
-#        if output == b'' and child.poll() != None:
-#            break
-#        if output != b'' and output != b'\r\n':
-#            output = output.decode("UTF-8")
-#            output_noeol = str(output).replace('\r', '')
-#            output_noeol = str(output_noeol).replace('\n', '')
-#            #print(output_noeol)
-#            if "not recognized as an internal or external command" in output_noeol:
-#                sub_skip = 1
-#            if "The system cannot find the path specified" in output_noeol:
-#                sub_skip = 1
-
-    print("finished child.pull time: ", time.time())
-    #rc = ctypes.c_long(child.poll()).value
 
     if sub_skip:
         rc = 2
@@ -182,14 +106,6 @@ def main():
         stage_report[0]['status'] = 'FAILED'
         stage_report[1]['log'].append('subprocess FAILED')
 
-    #monitor.terminate()
-    print("monitor terminated time: ", time.time())
-
-#    if real_rc.value != 0:
-#        rc = real_rc.value
-#        stage_report[0]['status'] = 'FAILED'
-#        stage_report[1]['log'].append('subprocess was terminated by monitor')
-
     with open(os.path.join(args.output, args.stage_report), 'w') as file:
         json.dump(stage_report, file, indent=' ')
 
@@ -204,7 +120,6 @@ def check_count():
     path_name = work_dir + "image_list.txt"
     with open(os.path.join(os.path.dirname(sys.argv[0]), path_name)) as f:
         images = f.read()
-        f.closed
 
     image_list = images.split("\n")
     # print (image_list)
@@ -222,8 +137,5 @@ def check_count():
 
 if __name__ == "__main__":
     rc = main()
-    #os.remove("tahoe.log")
-    #os.rmdir("cache")
-    print("start check_count time: ", time.time())
     check_count()
     exit(rc)
