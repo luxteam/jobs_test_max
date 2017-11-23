58f74dcf836460164224f71779326d2343ec9815
Andrey Alekseevich <magister.demens@gmail.com>
2017-11-23 18:22:37 +0300

    temp commit

--- scripts/run.bat
+++ scripts/run.bat
@@ -2,5 +2,6 @@ set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 set HWProfiles=CPU+WX7100
 set TestsFilter=small
 set Tool=2017
+set ConfigPath=%CD%\Max.config.json
 
 python ..\..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Results --work_dir Max
