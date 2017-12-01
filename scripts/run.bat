4632a026442400a6397329d38a3df3feb8f052ae
Andrey Alekseevich <magister.demens@gmail.com>
2017-12-01 13:39:14 +0300

    added submodule

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,3 +1,3 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 
-python ..\..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2017\3dsmax.exe" RenderDevice 2 TestsFilter full
+python ..\jobs_launcher\executeTests.py --tests_root ..\jobs --work_root ..\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2017\3dsmax.exe" RenderDevice 2 TestsFilter small
