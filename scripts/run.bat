cad65a07d4e0589ce570191865213f22ad68531c
Kravchenko Andrey <epozine@inbox.ru>
2018-04-20 14:34:01 +0300

    Add info in json & resolution param

--- scripts/run.bat
+++ scripts/run.bat
@@ -5,6 +5,6 @@ set TEST_PACKAGE=%3
 if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=2
 if "%TESTS_FILTER%" EQU "" set TESTS_FILTER=full
 
-python ..\jobs_launcher\executeTests.py --test_package Smoke_Test --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2017\3dsmax.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "C:\TestResources\MaxAssets\scenes"
+python ..\jobs_launcher\executeTests.py --test_package Camera_Test --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2017\3dsmax.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "C:\TestResources\MaxAssets\scenes" PassLimit 10 rx 0 ry 0
 
 pause
