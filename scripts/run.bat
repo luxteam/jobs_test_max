5a5cdc23f2ae1261bf42ea93e2ea3f00fd4bf384
Kravchenko Andrey <epozine@inbox.ru>
2018-05-04 15:21:55 +0300

    add test case; denoiser fix

--- scripts/run.bat
+++ scripts/run.bat
@@ -4,7 +4,7 @@ set TESTS_FILTER=%2
 set TEST_PACKAGE=%3
 if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=2
 if "%TESTS_FILTER%" EQU "" set TESTS_FILTER=full
-if "%TEST_PACKAGE%" EQU "" set TEST_PACKAGE=Full
+if "%TEST_PACKAGE%" EQU "" set TEST_PACKAGE=Test
 python ..\jobs_launcher\executeTests.py --file_filter %TEST_PACKAGE% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2017\3dsmax.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "C:\TestResources\MaxAssets\scenes" PassLimit 10 rx 0 ry 0
 
 pause
