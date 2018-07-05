34128f5a04dbc560ede94b82fb3d3ac053c04d31
Andrey Alekseevich <magister.demens@gmail.com>
2018-07-05 17:59:13 +0300

    added multi-filters

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,8 +1,7 @@
 set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 set RENDER_DEVICE=%1
-set TESTS_FILTER=%2
-set TEST_PACKAGE=%3
+set FILE_FILTER=%2
+set TESTS_FILTER="%3"
 if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=2
-if "%TESTS_FILTER%" EQU "" set TESTS_FILTER=full
-if "%TEST_PACKAGE%" EQU "" set TEST_PACKAGE=smoke
-python ..\jobs_launcher\executeTests.py --file_filter %TEST_PACKAGE% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2017\3dsmax.exe" RenderDevice %RENDER_DEVICE% TestsFilter %TESTS_FILTER% ResPath "C:\TestResources\MaxAssets\scenes" PassLimit 100 rx 0 ry 0
+if "%FILE_FILTER%" EQU "" set FILE_FILTER=smoke
+python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2017\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MaxAssets\scenes" PassLimit 100 rx 0 ry 0
