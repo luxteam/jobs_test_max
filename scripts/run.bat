626439de1973c5bc659a48282d54e735de3085bf
Andrey Alekseevich <magister.demens@gmail.com>
2018-08-01 15:22:02 +0300

    add split execution

--- scripts/run.bat
+++ scripts/run.bat
@@ -4,4 +4,5 @@ set FILE_FILTER=%2
 set TESTS_FILTER="%3"
 if "%RENDER_DEVICE%" EQU "" set RENDER_DEVICE=2
 if "%FILE_FILTER%" EQU "" set FILE_FILTER=smoke
-python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2017\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MaxAssets\scenes" PassLimit 100 rx 0 ry 0
+
+python ..\jobs_launcher\executeTests.py --split_execution %4 --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2017\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MaxAssets\scenes" PassLimit 50 rx 0 ry 0
