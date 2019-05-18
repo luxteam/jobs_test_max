e35dc7fa8f505521b3fd9f64e6c0f353ab3f9418
Dmitry Galkin <galkin.dmitry.485@gmail.com>
2019-05-18 13:29:52 +0300

    report creation

--- jobs/Scripts/ms_json.py
+++ jobs/Scripts/ms_json.py
@@ -3,30 +3,45 @@ import pymxs
 
 rt = pymxs.runtime
 
-class Dictionary:
-	def __init__(self, dictionary, filename):
-		self.dictionary = dictionary
+class JSONWrapper:
+	def __init__(self, data, filename):
+		self.data = data
 		self.filename = filename
 
 
 	def dump(self):
 		with open(self.filename, "w") as f:
-			json.dump(self.dictionary, f, indent=4)
+			json.dump([self.data], f, indent=4)
+		
 		return True
 
-	def get(self):
-		return self.dictionary
+
+	def get_data(self):
+		return self.data
+
+
+	def set_data(self, data):
+		self.data = data
+		return True
+
+
+	def get_filename(self):
+		return self.filename
+
+
+	def set_filename(self, filename):
+		self.filename = filename
+		return True
 
 
 	def setCaseStatus(self, index, value):
-		
-		self.dictionary["cases"][index - 1]["status"] = value
+		self.data["cases"][index - 1]["status"] = value
 		self.dump()
-
 		return True
 
 
 with open("case_list.json") as file:
 	data = json.loads(file.read())
 
-rt.caseList = Dictionary(data, "case_list.json")
+rt.caseList = JSONWrapper(data, "case_list.json")
+rt.report = JSONWrapper({}, None)
