03ba651d7bb1154957c3e9396ecd8ee5b7fe9519
Dmitry Galkin <galkin.dmitry.485@gmail.com>
2019-05-27 20:02:24 +0300

    refresh assets path

--- scripts/run.bat
+++ scripts/run.bat
@@ -3,11 +3,11 @@ set RENDER_DEVICE=%1
 set FILE_FILTER=%2
 set TESTS_FILTER="%3"
 
-python ..\jobs_launcher\executeTests.py %4 %5 --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2019\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MaxAssets\scenes" PassLimit 50 rx 0 ry 0
+python ..\jobs_launcher\executeTests.py %4 %5 --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2019\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\Assets\Smoke" PassLimit 50 rx 0 ry 0
 
 rem set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 rem set RENDER_DEVICE=2
 rem set FILE_FILTER=smoke
 rem set TESTS_FILTER="%3"
 
-rem python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2019\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MaxAssets\scenes" PassLimit 100 rx 0 ry 0
+rem python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2019\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\Assets\Smoke" PassLimit 100 rx 0 ry 0
