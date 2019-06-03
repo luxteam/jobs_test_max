3594bff16ed208c95f4c9f78c22337158c64ba0b
Dmitry Galkin <galkin.dmitry.485@gmail.com>
2019-06-03 16:32:48 +0300

    Pass_Limit cases

--- scripts/run.bat
+++ scripts/run.bat
@@ -7,7 +7,7 @@ python ..\jobs_launcher\executeTests.py %4 %5 --test_filter %TESTS_FILTER% --fil
 
 rem set PATH=c:\python35\;c:\python35\scripts\;%PATH%
 rem set RENDER_DEVICE=2
-rem set FILE_FILTER=smoke
+rem set FILE_FILTER=pass_limit
 rem set TESTS_FILTER="%3"
 
-rem python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2019\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\Assets\Smoke" PassLimit 100 rx 0 ry 0
+rem python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2019\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\Assets" PassLimit 100 rx 0 ry 0
