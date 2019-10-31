7e86373a1bbf490bcc11d04b2e6c6b4748f789e6
Andrey Alekseevich <magister.demens@gmail.com>
2019-10-31 10:03:03 +0300

    reduce pass limit

--- scripts/run.bat
+++ scripts/run.bat
@@ -5,4 +5,4 @@ set TESTS_FILTER="%3"
 
 python -m pip install -r ..\jobs_launcher\install\requirements.txt
 
-python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2019\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MaxAssets" PassLimit 50 rx 0 ry 0
+python ..\jobs_launcher\executeTests.py --test_filter %TESTS_FILTER% --file_filter %FILE_FILTER% --tests_root ..\jobs --work_root ..\Work\Results --work_dir Max --cmd_variables Tool "C:\Program Files\Autodesk\3ds Max 2019\3dsmax.exe" RenderDevice %RENDER_DEVICE% ResPath "C:\TestResources\MaxAssets" PassLimit 10 rx 0 ry 0
